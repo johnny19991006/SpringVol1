a9eb4d3cf092c403309fa88af62813d17db85dbb
JuYeong17 <dhwndud980@gmail.com>
2023-12-18 00:38:06 +0900

    AOP

---
+++ src/main/java/com/example/SpringVol1/TimeTraceAspect.java
@@ -0,0 +1,31 @@
+package com.example.SpringVol1;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Component
+@Aspect
+public class TimeTraceAspect {
+
+    // @Pointcut("execution(* com.example.demo.repository..*(..))")
+    @Pointcut("@annotation(com.example.demo.config.aop.TimeTrace)")
+    private void timeTracePointcut() {
+    }
+
+    @Around("timeTracePointcut()")
+    public Object traceTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        StopWatch stopWatch = new StopWatch();
+
+        try {
+            stopWatch.start();
+            return joinPoint.proceed(); // 실제 타겟 호출
+        } finally {
+            stopWatch.stop();
+
+        }
+    }
+}

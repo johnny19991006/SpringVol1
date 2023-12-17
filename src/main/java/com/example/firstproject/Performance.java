e0269e453cb1721f90f0575aecbec754f470f7b4
Jiae-Ham <coup31307@gmail.com>
2023-12-17 13:57:40 +0900

    AOP시간측정

--- src/main/java/com/example/firstproject/Performance.java
+++ src/main/java/com/example/firstproject/Performance.java
@@ -18,7 +18,7 @@ public class Performance {
             result = proceedingJoinPoint.proceed();
             long end = System.nanoTime();
 
-            System.out.println("수행 시간 : " + (end-start)/1000000.0);
+            System.out.println("수행 시간 : " + (end-start)/1000000.0 + "초");
         } catch (Throwable e) {
             System.out.println("exception!");
         }

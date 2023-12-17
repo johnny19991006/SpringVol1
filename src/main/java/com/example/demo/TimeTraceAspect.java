220e3c494b9ef69da5ed28f3ff83d80438cf9764
김성재 <103095432+seongjae6751@users.noreply.github.com>
2023-12-18 01:10:24 +0900

    aop

---
+++ src/main/java/com/example/demo/TimeTraceAspect.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Log4j2
+@Component
+@Aspect
+public class TimeTraceAspect {
+    // @Pointcut("execution(* com.example.demo.repository..*(..))")
+    @Pointcut("@annotation(com.example.demo.TimeTrace)")
+    private void timeTracePointcut() {
+    }
+
+    @Around("timeTracePointcut()")
+    public Object traceTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        StopWatch stopWatch = new StopWatch();
+
+        try {
+            stopWatch.start();
+            return joinPoint.proceed(); // 실제 타겟 호출
+        } finally {
+            stopWatch.stop();
+            log.debug("{} - Total time = {}s",
+                    joinPoint.getSignature().toShortString(),
+                    stopWatch.getTotalTimeSeconds());
+        }
+    }
+}
